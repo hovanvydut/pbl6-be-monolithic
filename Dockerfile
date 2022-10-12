178c23973f7be886f8eb5437c6f0ac806dbcb582
hovanvydut <hovanvydut@gmail.com>
2022-10-12 21:03:38 +0700

    [docker] enable Swagger when build image

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build-env
 WORKDIR /app
-    
+
 COPY *.csproj ./
 RUN dotnet restore
     
@@ -10,5 +10,6 @@ RUN dotnet publish -c Release -o out
 # Build runtime image
 FROM mcr.microsoft.com/dotnet/aspnet:6.0
 WORKDIR /app
+
 COPY --from=build-env /app/out .
 ENTRYPOINT ["dotnet", "monolithic.dll"]
--- Program.cs
+++ Program.cs
@@ -14,11 +14,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 

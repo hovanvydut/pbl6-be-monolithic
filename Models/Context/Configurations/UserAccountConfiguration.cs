1e7341c0ed011b9af0c0469c55cdec3e5280c549
phuocleoceo <phuoc.truong@emandai.net>
2022-10-23 23:07:48 +0700

    [Deploy] APIv28

--- Models/Context/Configurations/UserAccountConfiguration.cs
+++ Models/Context/Configurations/UserAccountConfiguration.cs
@@ -11,52 +11,52 @@ public class UserAccountConfiguration : IEntityTypeConfiguration<UserAccountEnti
     {
         builder.HasIndex(c => new { c.Email }).IsUnique();
 
-        var defaultPassword = "123456";
-        var passwordSecure = PasswordSecure.GetPasswordHash(defaultPassword);
-        var passwordHashed = passwordSecure.PasswordHashed;
-        var passwordSalt = passwordSecure.PasswordSalt;
-        builder.HasData
-        (
-            new UserAccountEntity
-            {
-                Id = 1,
-                Email = "ht10082001@gmail.com",
-                PasswordHashed = passwordHashed,
-                PasswordSalt = passwordSalt,
-                IsVerified = true,
-                RoleId = 1,
-                CreatedAt = DateTime.Now,
-            },
-            new UserAccountEntity
-            {
-                Id = 2,
-                Email = "tranphiphuong2763@gmail.com",
-                PasswordHashed = passwordHashed,
-                PasswordSalt = passwordSalt,
-                IsVerified = true,
-                RoleId = 1,
-                CreatedAt = DateTime.Now,
-            },
-            new UserAccountEntity
-            {
-                Id = 3,
-                Email = "dungngminh1311@gmail.com",
-                PasswordHashed = passwordHashed,
-                PasswordSalt = passwordSalt,
-                IsVerified = true,
-                RoleId = 1,
-                CreatedAt = DateTime.Now,
-            },
-            new UserAccountEntity
-            {
-                Id = 4,
-                Email = "hovanvydut@gmail.com",
-                PasswordHashed = passwordHashed,
-                PasswordSalt = passwordSalt,
-                IsVerified = true,
-                RoleId = 1,
-                CreatedAt = DateTime.Now,
-            }
-        );
+        // var defaultPassword = "123456";
+        // var passwordSecure = PasswordSecure.GetPasswordHash(defaultPassword);
+        // var passwordHashed = passwordSecure.PasswordHashed;
+        // var passwordSalt = passwordSecure.PasswordSalt;
+        // builder.HasData
+        // (
+        //     new UserAccountEntity
+        //     {
+        //         Id = 1,
+        //         Email = "ht10082001@gmail.com",
+        //         PasswordHashed = passwordHashed,
+        //         PasswordSalt = passwordSalt,
+        //         IsVerified = true,
+        //         RoleId = 1,
+        //         CreatedAt = DateTime.Now,
+        //     },
+        //     new UserAccountEntity
+        //     {
+        //         Id = 2,
+        //         Email = "tranphiphuong2763@gmail.com",
+        //         PasswordHashed = passwordHashed,
+        //         PasswordSalt = passwordSalt,
+        //         IsVerified = true,
+        //         RoleId = 1,
+        //         CreatedAt = DateTime.Now,
+        //     },
+        //     new UserAccountEntity
+        //     {
+        //         Id = 3,
+        //         Email = "dungngminh1311@gmail.com",
+        //         PasswordHashed = passwordHashed,
+        //         PasswordSalt = passwordSalt,
+        //         IsVerified = true,
+        //         RoleId = 1,
+        //         CreatedAt = DateTime.Now,
+        //     },
+        //     new UserAccountEntity
+        //     {
+        //         Id = 4,
+        //         Email = "hovanvydut@gmail.com",
+        //         PasswordHashed = passwordHashed,
+        //         PasswordSalt = passwordSalt,
+        //         IsVerified = true,
+        //         RoleId = 1,
+        //         CreatedAt = DateTime.Now,
+        //     }
+        // );
     }
 }
--- Models/Context/Configurations/UserProfileConfiguration.cs
+++ Models/Context/Configurations/UserProfileConfiguration.cs
@@ -10,60 +10,60 @@ public class UserProfileConfiguration : IEntityTypeConfiguration<UserProfileEnti
     {
         builder.HasIndex(c => new { c.PhoneNumber, c.IdentityNumber, c.UserAccountId }).IsUnique();
 
-        builder.HasData
-        (
-            new UserProfileEntity
-            {
-                Id = 1,
-                DisplayName = "Phuoc Truong",
-                PhoneNumber = "0382609982",
-                IdentityNumber = "197499999",
-                Avatar = "",
-                CurrentCredit = 0,
-                Address = "54 Nguyễn Lương Bằng",
-                AddressWardId = 6351,
-                UserAccountId = 1,
-                CreatedAt = DateTime.Now,
-            },
-            new UserProfileEntity
-            {
-                Id = 2,
-                DisplayName = "Phuong Tran",
-                PhoneNumber = "0336615425",
-                IdentityNumber = "012312312",
-                Avatar = "",
-                CurrentCredit = 0,
-                Address = "54 Nguyễn Lương Bằng",
-                AddressWardId = 6351,
-                UserAccountId = 2,
-                CreatedAt = DateTime.Now,
-            },
-            new UserProfileEntity
-            {
-                Id = 3,
-                DisplayName = "Dung Nguyen",
-                PhoneNumber = "0702479981",
-                IdentityNumber = "12345678",
-                Avatar = "",
-                CurrentCredit = 0,
-                Address = "54 Nguyễn Lương Bằng",
-                AddressWardId = 6351,
-                UserAccountId = 3,
-                CreatedAt = DateTime.Now,
-            },
-            new UserProfileEntity
-            {
-                Id = 4,
-                DisplayName = "Vy Ho",
-                PhoneNumber = "0123456789",
-                IdentityNumber = "12345678",
-                Avatar = "",
-                CurrentCredit = 0,
-                Address = "54 Nguyễn Lương Bằng",
-                AddressWardId = 6351,
-                UserAccountId = 4,
-                CreatedAt = DateTime.Now,
-            }
-        );
+        // builder.HasData
+        // (
+        //     new UserProfileEntity
+        //     {
+        //         Id = 1,
+        //         DisplayName = "Phuoc Truong",
+        //         PhoneNumber = "0382609982",
+        //         IdentityNumber = "197499999",
+        //         Avatar = "",
+        //         CurrentCredit = 0,
+        //         Address = "54 Nguyễn Lương Bằng",
+        //         AddressWardId = 6351,
+        //         UserAccountId = 1,
+        //         CreatedAt = DateTime.Now,
+        //     },
+        //     new UserProfileEntity
+        //     {
+        //         Id = 2,
+        //         DisplayName = "Phuong Tran",
+        //         PhoneNumber = "0336615425",
+        //         IdentityNumber = "012312312",
+        //         Avatar = "",
+        //         CurrentCredit = 0,
+        //         Address = "54 Nguyễn Lương Bằng",
+        //         AddressWardId = 6351,
+        //         UserAccountId = 2,
+        //         CreatedAt = DateTime.Now,
+        //     },
+        //     new UserProfileEntity
+        //     {
+        //         Id = 3,
+        //         DisplayName = "Dung Nguyen",
+        //         PhoneNumber = "0702479981",
+        //         IdentityNumber = "12345678",
+        //         Avatar = "",
+        //         CurrentCredit = 0,
+        //         Address = "54 Nguyễn Lương Bằng",
+        //         AddressWardId = 6351,
+        //         UserAccountId = 3,
+        //         CreatedAt = DateTime.Now,
+        //     },
+        //     new UserProfileEntity
+        //     {
+        //         Id = 4,
+        //         DisplayName = "Vy Ho",
+        //         PhoneNumber = "0123456789",
+        //         IdentityNumber = "12345568",
+        //         Avatar = "",
+        //         CurrentCredit = 0,
+        //         Address = "54 Nguyễn Lương Bằng",
+        //         AddressWardId = 6351,
+        //         UserAccountId = 4,
+        //         CreatedAt = DateTime.Now,
+        //     }
+        // );
     }
 }

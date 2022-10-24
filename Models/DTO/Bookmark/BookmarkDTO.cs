2b62385ad5e2ffb4b6af620ad0e24e3a27553410
phuocleoceo <phuoc.truong@emandai.net>
2022-10-24 16:14:39 +0700

    [Bookmark][BE] Model and API

--- Models/DTO/Bookmark/BookmarkDTO.cs
+++ Models/DTO/Bookmark/BookmarkDTO.cs
@@ -2,5 +2,15 @@ namespace Monolithic.Models.DTO;
 
 public class BookmarkDTO
 {
+    public int Id {get;set;}
 
+    public int PostId { get; set; }
+
+    public string PostTitle { get; set; }
+
+    public string PostDescription { get; set; }
+
+    public string PostPrice { get; set; }
+
+    public string PostSlug { get; set; }
 }
